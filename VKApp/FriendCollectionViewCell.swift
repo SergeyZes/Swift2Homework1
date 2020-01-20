bd31304253b2aa4d2d3e866aa14c2f94e9dd4af7
ZesMac <sergey_the_great@mail.ru>
2020-01-20 23:58:56 +0500

    Выполнил все пункты домашней работы.

---
+++ VKApp/FriendCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCollectionViewCell.swift
+//  VKApp
+//
+//  Created by Сергей Зесли on 20.01.2020.
+//  Copyright © 2020 Сергей Зесли. All rights reserved.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+}
